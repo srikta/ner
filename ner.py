43a0f9e5d9b2b16099ce5e2223820ae095c83b31
srikta <souriktanag@gmail.com>
2024-04-11 17:18:05 +0530

    This is nlp project

--- ner.py
+++ ner.py
@@ -8,7 +8,9 @@ import subprocess
 @st.cache_resource
 def download_en_core_web_sm():
     subprocess.run(["python","-m","spacy","download","en_core_web_sm"])
-    
+download_en_core_web_sm()   
+
+
 st.title("Named Entity Recognition")
 
 if st.button("About"):
